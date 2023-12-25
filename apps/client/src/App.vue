32e8c158556690416cce005e8927a31056cc3bbe
JohnGolgota <js684new@gmail.com>
2023-12-25 16:40:31 -0500

    Fuck vscode

---
+++ apps/client/src/App.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+const message = ref('Hello Vue 3!')
+const fetchMessage = async () => {
+    const res = await fetch('/api')
+    message.value = await res.text()
+}
+</script>
+
+<template>
+    <button @click="fetchMessage">
+        Click me
+    </button>
+    <div class="resp">
+        {{ message }}
+    </div>
+</template>
+
+<style scoped>
+.logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+}
+
+.logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+    filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
